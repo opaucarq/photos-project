b343ff29201deb188c38d0ece0ff22254da0450b
Oliver <opaucarq@uni.pe>
2023-07-27 16:21:52 -0500

    update: main App for adding photos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
-const API_URL = "http://localhost:3000"
+import GridPhotos from './components/GridPhotos';
+
+import { postData, getImages } from './services/service-image';
 
 function App() {
-  const [images, setImages] = useState([]);
+  const [data, setData] = useState([]);
   const imagesRef = useRef([])
-  const postToGet = useRef(1)
 
   const handleUpload = (e) => {
     e.preventDefault();
@@ -17,61 +16,43 @@ function App() {
     for (let i= 0; i< imagesRef.current.files.length;i++){
       formData.append("post[images][]", imagesRef.current.files[i])
     }
-    postData(formData)
+    postData(formData).then(() => {
+      fetchData(); 
+      imagesRef.current.value = "";
+    });
   }
 
-const postData = (formData) => {
-  fetch(`${API_URL}/posts`,{
-    method: "POST",
-    body: formData,
-  })
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    getImages();
-  })
-  .catch((err) => console.log(err))
-}
+  const fetchData = () => {
+    getImages()
+      .then((data) => {        
+        console.log(data)
 
-const getImages = () => {
-  fetch(`${API_URL}/posts/${postToGet.current.value}`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    setImages(data.images);
-  })
-  .catch((err) => console.log(err))
-}
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
     <>
       <div className='App'>
-        <form action="">
-          <input type="file" name="image" multiple ref={imagesRef} />
+        <form action=''>
+          <input type='file' name='image' multiple ref={imagesRef}  required/>
           <button type='button' onClick={handleUpload}>
             Submit
           </button>
         </form>
+
+        <hr />
         <div>
-          <input type="numer" ref={postToGet} placeholder="ID to retrieve"/>
-          <button onClick={getImages}>GetImages</button>
+          <GridPhotos data={data}/>
         </div>
-
-        <div className='images'>
-          {images.map((image,index) => (
-            <img 
-              key={index}
-              src={image}
-              alt="uploades"
-              style={{width:"200px", height: "200px"}}
-            />
-          ))}
-        </div>
-        
       </div>
-
     </>
-  )
+  );
 }
 
 export default App
